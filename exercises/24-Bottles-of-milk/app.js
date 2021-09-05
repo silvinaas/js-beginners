59ab10877c17325e1e27bacf72ab7d6f7c665c5f
silvinaas <silvinaas4@gmail.com>
2021-09-05 19:39:00 +0000

    ya esta pronto

--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,1 +1,16 @@
 // Your code here:
+function bottlesOfMilk(num, num2) {
+    let partOfTheSong = "";
+    for (let i = 0; i < 2; i++) {
+        partOfTheSong = (num + " bottles of milk on the wall, " + num + " bottles of milk. Take one down and pass it around, " + num2 + " bottles of milk on the wall. ")
+ 
+    }
+    return partOfTheSong;
+} 
+
+function song(){
+    entireSong = bottlesOfMilk(99, 98) + bottlesOfMilk (98,97) + "1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall." + "No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.";
+return entireSong;
+}
+
+console.log (song());
